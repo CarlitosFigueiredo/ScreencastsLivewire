4815d0b169b0235f16e5d96f17b84ada5a0923f6
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2023-11-28 10:31:41 -0300

    responsive +_

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -5,35 +5,30 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+    <div class="flex justify-center mt-2">
+        <div class="flex flex-col gap-8 w-[50rem] px-8 text-gray-800">
+            <table class="min-w-full divide-y divide-slate-300 bg-white shadow rounded-xl">
+                <thead>
+                    <tr>
+                        <th class="border border-slate-600 p-2 bg-white dark:bg-gray-800 w-20 text-start">
+                            {{ __('Title') }}
+                        </th>
+                        <th class="border border-slate-600 p-2 bg-white dark:bg-gray-800 text-start">
+                            {{ __('Content') }}
+                        </th>
+                        <th class="border border-slate-600 p-2 bg-white dark:bg-gray-800 w-48 text-gray-800">
+                        
+                            <livewire:add-post-dialog @added="$refresh" />
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
 
-                    <table class="w-full table-fixed border-separate border-spacing-2 border border-slate-500 hover:border-spacing-2">
-                        <thead>
-                            <tr>
-                                <th class="border border-slate-600 p-2 bg-white dark:bg-gray-800 w-20 text-start">
-                                    {{ __('Title') }}
-                                </th>
-                                <th class="border border-slate-600 p-2 bg-white dark:bg-gray-800 text-start">
-                                    {{ __('Content') }}
-                                </th>
-                                <th class="border border-slate-600 p-2 bg-white dark:bg-gray-800 w-48 text-gray-800">
-                                
-                                    <livewire:add-post-dialog @added="$refresh" />
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($posts as $post)
-
-                                <livewire:post-row :key="$post->id" :post="$post" />
-                            @endforeach
-                        </tbody>
-                    </table> 
-                </div>
-            </div>
+                        <livewire:post-row :key="$post->id" :post="$post" />
+                    @endforeach
+                </tbody>
+            </table> 
         </div>
     </div>
 </div>
